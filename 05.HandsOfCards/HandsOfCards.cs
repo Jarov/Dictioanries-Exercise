10836a021ead1178a27849f888320992513f8c8d
Yavor <qvalkov@gmail.com>
2017-02-05 20:23:58 +0200

    Fixed the FixEmails app and added HandsOfCards app, Dragons Army app.

---
+++ 05.HandsOfCards/HandsOfCards.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HandsOfCards
+{
+    class HandsOfCards
+    {
+        static void Main()
+        {
+            Dictionary<string, HashSet<string>> PlayersHands = new Dictionary<string, HashSet<string>>();
+            //Речник със който пазим за всеки играч всички уникални карти които е истеглил
+
+            for (int loop = 0; ; loop++)
+            {
+                string[] inPut = Console.ReadLine().Split(':');  //inPut[0] - името на играчът, inPut[1] - Картите които е истеглил
+
+                if (inPut[0] == "JOKER") break;
+
+                List<string> CurentHand = inPut[1].Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+                //прехвърляме картите които играчът е истеглил в момента в лист за да можем по лесно да ги добавим в HashSet който ще вземе само уникалните карти които играчът е истеглил 
+
+                if (!PlayersHands.ContainsKey(inPut[0]))  //ако нямаме регистриран играч в речника създаваме HashSet от карти за играчът
+                    PlayersHands[inPut[0]] = new HashSet<string>();
+
+                foreach (string card in CurentHand)  //добавяме картите в HashSet-а който сме създали за дадения играч
+                    PlayersHands[inPut[0]].Add(card);
+            }
+
+            foreach (KeyValuePair<string, HashSet<string>> player in PlayersHands)
+                Console.WriteLine($"{player.Key}: {CalculateScore(player.Value)}");
+            //player.Key - името на играчът, player.Value - HashSet от всички карти които е взел
+            //вместо да правим новa променлива която да пази името на играча и неговият резултат директно принтираме резултата без да го пазим (грешен подход ако се изисква после да се правят други неща със резултата)
+        }
+
+        static int CalculateScore(HashSet<string> Cards)
+        {
+            int sum = 0;
+
+            foreach (string card in Cards)
+            {
+                byte power = 0;
+                switch (card.Substring(0, card.Length - 1)) //Taking the power of the current card
+                {
+                    case "2": power = 2; break;
+                    case "3": power = 3; break;
+                    case "4": power = 4; break;
+                    case "5": power = 5; break;
+                    case "6": power = 6; break;
+                    case "7": power = 7; break;
+                    case "8": power = 8; break;
+                    case "9": power = 9; break;
+                    case "10": power = 10; break;
+                    case "J": power = 11; break; //Jack
+                    case "Q": power = 12; break; //Queen
+                    case "K": power = 13; break; //King
+                    case "A": power = 14; break; //Ace
+                }
+
+                byte type = 0;
+
+                switch (card.Substring(card.Length - 1))  //Taking the type of the current card
+                {
+                    case "S": type = 4; break; //Spades
+                    case "H": type = 3; break; //Hearths
+                    case "D": type = 2; break; //Diamonds 
+                    case "C": type = 1; break; //Clubs
+                }
+
+                sum += power * type;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ 11.DragonsArmy/Dragons Army.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dragons_Army
+{
+    class StatsOfDragons
+    {
+        public int Damage;
+        public int Health;
+        public int Armor;
+        
+        public StatsOfDragons(string dmg, string hp, string ar)
+        {
+            if (dmg == "null")
+                Damage = 45;
+            else
+                Damage = int.Parse(dmg);
+            if (hp == "null")
+                Health = 250;
+            else
+                Health = int.Parse(hp);
+            if (ar == "null")
+                Armor = 10;
+            else
+                Armor = int.Parse(ar);
+        }
+        
+    }
+
+    class Dragons_Army
+    {
+        static void Main()
+        {
+            byte NumberOfDragons = byte.Parse(Console.ReadLine());
+
+            Dictionary<string, SortedDictionary<string, StatsOfDragons>> Dragons = new Dictionary<string, SortedDictionary<string, StatsOfDragons>>();
+
+            InsertTheDragons(Dragons, NumberOfDragons);
+
+            foreach (string type in Dragons.Keys)
+            {
+                double armor = 0;
+                double health = 0;
+                double damage = 0;
+
+                foreach (string name in Dragons[type].Keys)
+                {
+                    armor += Dragons[type][name].Armor;
+                    health += Dragons[type][name].Health;
+                    damage += Dragons[type][name].Damage;
+                }
+
+                armor = armor / Dragons[type].Count;
+                health = health / Dragons[type].Count;
+                damage = damage / Dragons[type].Count;
+
+                Console.WriteLine($"{type}::({damage.ToString("0.00")}/{health.ToString("0.00")}/{armor.ToString("0.00")})");
+                foreach (string name in Dragons[type].Keys)
+                {
+                    Console.WriteLine($"-{name} -> damage: {Dragons[type][name].Damage}, health: {Dragons[type][name].Health}, armor: {Dragons[type][name].Armor}");
+                }                    
+            }
+        }
+
+        static void InsertTheDragons(Dictionary<string, SortedDictionary<string, StatsOfDragons>> Dragons, int NumberOfDragons)
+        {
+            for (int i = 0; i < NumberOfDragons; i++)
+            {
+                string[] inPut = Console.ReadLine().Split(' ');
+
+                if (!Dragons.ContainsKey(inPut[0]))
+                {
+                    Dragons[inPut[0]] = new SortedDictionary<string, StatsOfDragons>();
+                }
+                Dragons[inPut[0]][inPut[1]] = new StatsOfDragons(inPut[2], inPut[3], inPut[4]);
+            }
+        }
+    }
+}
