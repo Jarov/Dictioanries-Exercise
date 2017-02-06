a9b4ef38b3c5d7c1621124bb457a1c24913f4679
Yavor <qvalkov@gmail.com>
2017-02-06 02:26:14 +0200

    I made UserLogs app and PopulationCounter app. I have to find a way to sort the countries by their total population and the cities by their population.

---
+++ 06.UserLogs/UserLogs.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.UserLogs
+{
+    class UserLogs
+    {
+        static void Main()
+        {
+            SortedDictionary<string, Dictionary<string, short>> UserIPAddresses = new SortedDictionary<string, Dictionary<string, short>>();
+
+            for (byte i = 0; i < 50; i++)
+            {
+                string[] inPut = Console.ReadLine().Split(new char[] { ' ', '=' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (inPut[0] == "end")
+                {
+                    Print(UserIPAddresses);
+                    break;
+                }
+
+                string IP = inPut[1];
+                string user = inPut[5];
+
+                if (!UserIPAddresses.ContainsKey(user))
+                {
+                    UserIPAddresses[user] = new Dictionary<string, short>();
+                }
+
+                if (!UserIPAddresses[user].ContainsKey(IP))
+                {
+                    UserIPAddresses[user][IP] = 0;
+                }
+
+                UserIPAddresses[user][IP] += 1;
+            }
+        }
+
+        static void Print(SortedDictionary<string, Dictionary<string, short>> UserIPAddresses)
+        {
+            foreach (KeyValuePair<string, Dictionary<string, short>> User in UserIPAddresses)
+            {
+                Console.WriteLine($"{User.Key}: ");
+                foreach (KeyValuePair<string, short> IPAddress in UserIPAddresses[User.Key])
+                {
+                    Console.Write($"{IPAddress.Key} => {IPAddress.Value}");
+                    if (IPAddress.Key == UserIPAddresses[User.Key].Last().Key)
+                    {
+                        Console.WriteLine(".");
+                    }
+                    else
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 07.PopulationCounter/PopulationCounter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PopulationCounter
+{
+    class PopulationCounter
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, long>> Countries = new Dictionary<string, Dictionary<string, long>>();
+            
+            for (int loop = 0; ; loop++)
+            {
+                string[] inPut = Console.ReadLine().Split('|');
+
+                if (inPut[0] == "report")
+                {
+                    foreach (var Country in Countries)
+                    {
+                        long Population = Countries[Country.Key].Values.Sum();
+
+                        Console.WriteLine($"{Country.Key} (total population: {Population})");
+                        foreach (var City in Countries[Country.Key])
+                        {
+                            Console.WriteLine($"=>{City.Key}: {Countries[Country.Key][City.Key]}");
+                        }
+                    }
+
+                    break;
+                }
+
+                if (!Countries.ContainsKey(inPut[1]))
+                    Countries[inPut[1]] = new Dictionary<string, long>();
+                if (!Countries.ContainsKey(inPut[0]))
+                    Countries[inPut[1]][inPut[0]] = 0;
+
+                Countries[inPut[1]][inPut[0]] = long.Parse(inPut[2]);
+            }
+        }
+    }
+}
